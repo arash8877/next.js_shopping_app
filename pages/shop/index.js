e38c0147de5efdf24b68432daf72814a108f97d5
Arash <arash8877@gmail.com>
2024-01-08 20:39:40 +0100

    added shop/index.js

---
+++ pages/shop/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+
+
+const Index = (props) => {
+
+  return (
+    <div>
+        <ul>
+            <li><Link href={"/shop/product/111"}>product 1</Link></li>
+            <li><Link href={"/shop/product/222"}>product 2</Link></li>
+            <li><Link href={"/shop/product/333"}>product 3</Link></li>
+            <li><Link href={"/shop/product/444"}>product 4</Link></li>
+            <li><Link href={"/shop/product/555"}>product 5</Link></li>
+            <li><Link href={"/shop/product/666"}>product 6</Link></li>
+            <li><Link href={"/shop/product/777"}>product 7</Link></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Index;
